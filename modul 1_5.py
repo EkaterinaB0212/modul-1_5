ae002230fc6152ea37d4fa5685e2ec8426f936b6
EkaterinaB0212 <neugodova_e.a@mail.ru>
2024-08-01 15:43:08 +0300

    Add files via upload

---
+++ modul 1_5.py
@@ -0,0 +1,9 @@
+immutable_var = 7, 'book', 9
+print(immutable_var)
+#immutable_var[0] = 9
+#print(immutable_var)
+mutable_list = [7,'book', 9]
+mutable_list[0] = 5
+mutable_list[1] = 'copybook'
+mutable_list[2] = 3
+print(mutable_list)
